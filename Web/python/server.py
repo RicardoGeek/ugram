bf9b2d07e0914cbcea22ff3d48a3466bbba7c0b6
RicardoGeek <mastermind401@gmail.com>
2021-03-06 09:21:55 -0600

    python health endpoint

--- Web/python/server.py
+++ Web/python/server.py
@@ -2,6 +2,12 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+@app.route("/health")
+def health():
+    return {
+        "status": "up"
+    }
+
 import user_routes
 import album_routes
 import photo_routes
