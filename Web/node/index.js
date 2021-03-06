16c41530a5044e7d42868ac00ce273e1d75205f6
joseEstrada <jose.estrada@mqaamericas.com>
2021-03-06 12:04:35 -0600

    health endpoint

--- Web/node/index.js
+++ Web/node/index.js
@@ -23,3 +23,9 @@ app.use('/auth', userController.authUser);
 app.listen(3000, () => {
   console.log('Listening on port:  ' + 3000);
 });
+
+app.get('/health', (req, res) => {
+  res.send({
+    "status": "up"
+  })
+})
